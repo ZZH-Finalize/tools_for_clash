6247d79268bf2db218b15379d3557ed015cb2c13
ZZH <1462207500@qq.com>
2024-05-15 10:49:14 +0100

    update get_proxy
    Signed-off-by: ZZH <1462207500@qq.com>

--- src/bin/get_proxy.rs
+++ src/bin/get_proxy.rs
@@ -1,19 +1,66 @@
-use std::{fs::File, io::Write, process::ExitCode};
+use chrono::Local;
+use reqwest::{
+    self,
+    blocking::Client,
+    header::{HeaderMap, HeaderValue},
+    Method, Url,
+};
+use serde_json::Value;
+use std::{cmp::Ordering, fs::File, io::Write, process::ExitCode, str::FromStr};
 
-use reqwest;
+// "Accept: */*",
+// "Accept-Encoding: gzip, deflate, br, zstd",
+// "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,en-GB;q=0.5",
+// "Dnt: 1",
+// "Priority: u=1, i",
+// "Referer: https://checkerproxy.net/archive/2024-05-15",
+// "Sec-Ch-Ua: "Chromium";v="124", "Microsoft Edge";v="124", "Not-A.Brand";v="99"",
+// "Sec-Ch-Ua-Mobile: ?0",
+// "Sec-Ch-Ua-Platform: "Windows"",
+// "Sec-Fetch-Dest: empty",
+// "Sec-Fetch-Mode: cors",
+// "Sec-Fetch-Site: same-origin",
+// "Sec-Gpc: 1",
+// "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
+// "Host: checkerproxy.net",
+// const HEADERS: [&str; 2] = [
+//     "Referer: https://checkerproxy.net/archive/",
+//     "Host: checkerproxy.net",
+// ];
 
+pub fn cmp_by_timeout(x: &Value, y: &Value) -> Ordering {
+    let x_tm = x.as_u64().unwrap();
+    let y_tm = y.as_u64().unwrap();
 
-pub fn main() -> ExitCode{
-    // let url = "https://checkerproxy.net/archive/2024-04-16";
-    // let url = "https://checkerproxy.net/dist/app.57112776e884f15d8518.js";
-    let url = "https://checkerproxy.net/dist/vendor.f77040baef66ee31ae0d.js";
-    let get_res = reqwest::blocking::get(url).unwrap();
+    if x_tm > y_tm {
+        Ordering::Greater
+    } else {
+        Ordering::Less
+    }
+}
 
-    let mut file = File::create("vendor.js").unwrap();
+pub fn main() -> ExitCode {
+    let url = String::from_str("https://checkerproxy.net").unwrap();
+    let now = Local::now().format("%Y-%m-%d");
+    let api_url = url.clone() + "/api/archive/" + &now.to_string();
+    let ref_url = url.clone() + "/archive/" + &now.to_string();
 
-    file.write_all(get_res.text().unwrap().as_bytes()).unwrap();
+    let mut headers = HeaderMap::new();
+    headers.append("Referer", HeaderValue::from_str(ref_url.as_str()).unwrap());
+    headers.append("Host", HeaderValue::from_str("checkerproxy.net").unwrap());
 
-    // println!("{:#?}", get_res.text().unwrap());
+    let client = Client::new();
+    let resp = client.get(api_url).headers(headers).send();
+
+    match resp {
+        Ok(data) => {
+            let proxies_data = data.json::<serde_json::Value>().unwrap();
+            let proxies = proxies_data.as_array().unwrap();
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
 
     ExitCode::SUCCESS
 }
